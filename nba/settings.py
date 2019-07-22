67b581addca52b8a7c4fbf2256918ff5c000f815
TsaiWenXue <wto2005716@gmail.com>
2019-07-22 15:17:10 +0800

    add custom pagination

--- nba/settings.py
+++ nba/settings.py
@@ -131,6 +131,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'news.pagination.CustomPageNumber',
     'PAGE_SIZE': 10
 }
